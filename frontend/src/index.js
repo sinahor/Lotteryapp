43bb9747fcb71e80e301886ae89790e1a907e825
visakhpootheri <visakhpootheri97@gmail.com>
2022-11-24 12:23:12 +0530

    pathedit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navigation from './components/List';
+import Navigation from './Navigation';
 import App from './components/Option';
 // import VerifyOTP from './pages/VerifyOTP';
 
