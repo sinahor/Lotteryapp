50f3ca5e2f43fc9bac054c27f87ffb7e477de9db
VidhyaSugathan <118793482+VidhyaSugathan@users.noreply.github.com>
2022-11-24 11:04:40 +0530

    Create TicketSelector.js

---
+++ frontend/src/pages/TicketSelector.js
@@ -0,0 +1,9 @@
+import header from "../components/HeaderUser";
+import drawinformation from "../components/Drawresult";
+import lineselector from "../components/Lineselector";
+import linemessage from "../components/Line"
+export default function(){
+    return(
+
+    )
+}
