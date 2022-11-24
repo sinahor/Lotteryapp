b63e6419776c8e2532092730bf09ed403b8ab1cb
VidhyaSugathan <118793482+VidhyaSugathan@users.noreply.github.com>
2022-11-24 14:46:52 +0530

    TicketSelector
    Page created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navigation from './Navigation'
+// import Navigation from './Navigation/TicketSelector';
+import Navigation from './pages/TicketSelector';
+import App from './components/Option';
 // import VerifyOTP from './pages/VerifyOTP';
 
 
--- frontend/src/pages/TicketSelector.js
+++ frontend/src/pages/TicketSelector.js
@@ -1,9 +1,17 @@
-import header from "../components/HeaderUser";
-import drawinformation from "../components/Drawresult";
-import lineselector from "../components/Lineselector";
-import linemessage from "../components/Line"
-export default function(){
+import Header from "../components/HeaderUser";
+import Drawinformation from "../components/Drawresult";
+import Lineselector from "../components/Lineselector";
+import Linemessage from "../components/Linemessage";
+import Footer from "../components/Footer";
+export default function TicketSelector(){
     return(
 
-    )
+        <>
+        <Header/>
+        <Drawinformation/>
+        <Lineselector/>
+        <Linemessage/>
+        <Footer/>
+        </>
+    );
 }
